67d948fe21b0f2a4637de7bf9906fb33b99b5971
王治尧 <1217167460@qq.com>
2020-01-08 18:30:12 +0800

    增加装饰器

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <button @click="startworker()"></button>
+    <button @click="startworker()">startworker</button>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
 @Component({
   components: { HelloWorld }
 })
