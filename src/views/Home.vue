7793ca76e4e42fdf616b5eff8550f381ef812c6b
王治尧 <1217167460@qq.com>
2020-01-08 18:21:54 +0800

    first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button @click="startworker()"></button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+@Component({
+  components: { HelloWorld }
+})
+export default class Home extends Vue {
+  startworker() {
+    if (typeof Worker !== "undefined") {
+      let worker = new Worker("../../static/webworker.js"); // 需要每次都调用打开一个woker
+      // this.worker = new Worker('@/assets/js/webworker.js'); // 需要每次都调用打开一个woker
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+      // 可以序列化为JSON结构的任何值都可以作为参数传递给 postMessage();
+      // this.worker.postMessage(msg);
+      // this.worker.onmessage = event => {
+      //     this.worker_data = event.data;
+      // };
+      console.log("start Web Workers...");
+    } else {
+      console.log("Sorry, your browser does not support Web Workers...");
+    }
   }
 }
 </script>
